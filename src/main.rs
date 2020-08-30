c3f8953021c199a81c351a59fcd4f6810d1c5205
Boris Vinogradov <no111u3@gmail.com>
2020-08-30 12:17:13 +0300

    Update main.rs to current project state

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-#![feature(llvm_asm)]
 #![no_std]
 #![no_main]
 
 extern crate panic_halt;
 
-#[avr_device::entry]
+use m48_robo_rust::prelude::*;
+
+#[m48_robo_rust::entry]
 fn main() -> ! {
+    let dp = m48_robo_rust::Peripherals::take().unwrap();
+
+    let mut pinsd = dp.PORTD.split();
+
+    let mut serial = m48_robo_rust::Serial::new(
+        dp.USART0,
+        pinsd.pd0,
+        pinsd.pd1.into_output(&mut pinsd.ddr),
+        2400,
+    );
+
+    ufmt::uwriteln!(&mut serial, "Echo from ATmega48P!\r").void_unwrap();
+
     loop {}
 }
